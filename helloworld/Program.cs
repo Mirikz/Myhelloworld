136845d9b1c1e3d5da995c2c1abd0255ed2283a2
Mirikz <kirillmitrofanov68@gmail.com>
2022-01-12 10:22:33 +0500

    first project

---
+++ helloworld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Oh, hi, Mark");
+Console.WriteLine("Anyway, how is your sex life?");
+Console.ReadKey();
